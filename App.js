9c05b2842ff02a60e0143b7579a216ef2892348b
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-04-10 23:40:06 +0530

    Added FlatList and Snap Items

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem
-        name={'Model S'}
-        tagline={'$xx,xxx'}
-        taglineCTA={'touchless delivery'}
-        image={require('./assets/images/ModelS.jpeg')}
-      />
+      <CarsList />
 
       <StatusBar style="auto" />
     </View>
